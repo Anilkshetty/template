78b3d36853e77ad883e52b9265f24214fe519bb4
Anilkshetty <43844289+Anilkshetty@users.noreply.github.com>
2018-10-04 15:43:50 +0530

    Add files via upload

---
+++ example.js
@@ -0,0 +1,13 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250";
+    document.getElementById("mynavbar").style.display = "none";
+
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mynavbar").style.display = "block";
+   
+}
